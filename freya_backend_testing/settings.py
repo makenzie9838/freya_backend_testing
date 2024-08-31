b7604016f77e9a4c4526e1a260dd9d361b7917c4
Mackenzie Eng <86166064+makenzie9838@users.noreply.github.com>
2024-08-30 20:20:09 -0400

    Provision PostgreSQL database on AWS

--- freya_backend_testing/settings.py
+++ freya_backend_testing/settings.py
@@ -74,13 +74,32 @@ WSGI_APPLICATION = 'freya_backend_testing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': 'freya',
+
+        'USER': 'mackenzie',
+
+        'PASSWORD': 'adhvby6eB6Qbx0Fn4Tjj',
+
+        'HOST': 'freya.czkimoqom091.us-east-2.rds.amazonaws.com',
+
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
